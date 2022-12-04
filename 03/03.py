a64b6458fed012046a6687c6872ec8672b32896e
Yelyzaveta Tarabarova <ltarabarova@playwire.com>
2022-12-04 15:40:59 +0200

    remove extra

--- 03/03.py
+++ 03/03.py
@@ -3,21 +3,12 @@ import string
 def list_comparison(rucksack):
     first_part = []
     second_part = []
-
     rucksack_list = list(rucksack)
     length = len(rucksack_list)//2
-
-    for x in rucksack_list[:length]:
-        first_part.append(x)
-
-    for x in rucksack_list[length:]:
-        second_part.append(x)
-
-    # first_part, second_part = rucksack_list[:length], rucksack_list[length:] 
+    first_part, second_part = rucksack_list[:length], rucksack_list[length:] 
 
     match = (set(first_part) & set(second_part))
     items = list(string.ascii_letters)
-
     return items.index(next(iter(match)))+1
 
 def three_list_comparison(rucksack_group):
